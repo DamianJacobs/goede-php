4179cfcb652ad9c61cd9bfbc58b772188116b1ca
DamianJ <99046909@mydavinci.nl>
2018-03-26 10:16:01 +0200

    plzz??

---
+++ index.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<link rel="stylesheet" href="goede.css" type="text/css">
+	<title>Tijd</title>
+</head>
+<body id="ochtend">
+	<?php
+	echo date("l jS \of F Y H:i:s");
+	?>
+	<p><?php
+$t = date("H");
+
+if ($t < "12") {
+    echo "Goedemorgen";
+		echo '<body style="background-image: url(backgrounds/morning.png);"';
+} elseif ($t < "18") {
+	echo "goedemiddag";
+	echo '<body style="background-image: url(backgrounds/afternoon.png);"';
+} elseif ($t < "24") {
+	echo "goedeAvond";
+	echo '<body style="background-image: url(backgrounds/evening.png);"';
+} elseif ($t < "6") {
+	echo "goedeNacht";
+	echo '<body style="background-image: url(backgrounds/night.png);"';
+}
+?></p>
+</body>
+</html>
